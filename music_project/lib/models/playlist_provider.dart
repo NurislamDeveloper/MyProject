4352ae8b333dc39b567cdfd24ab79931e7c33d32
Nurislam <nurislam.temirbekov.bj@gmail.com>
2024-04-04 14:06:09 +0600

    it has the some problems

--- music_project/lib/models/playlist_provider.dart
+++ music_project/lib/models/playlist_provider.dart
@@ -1,37 +1,175 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:music_project/models/song.dart';
 
 class PlaylistProvider extends ChangeNotifier {
-  final List<Song> _playlist = [
+  //playlist of song
+  final List<Song> _playlist = [ 
+
     Song(
       albumArtImagePath: "assets/image/miagi1.jpg",
-      audioPath: "song/MiyaGi - Angel.mp3",
+      audioPath: "song/MiyaGi.mp3",
       songName: "Angel",
       artistName: "M I A G I",
     ),
     Song(
       albumArtImagePath: "assets/image/miagi2.jpg",
-      audioPath: "song/MiyaGi - Angel.mp3",
+      audioPath: "song/MiyaGi.mp3",
       songName: "Samuray",
       artistName: "M I A G I",
     ),
     Song(
       albumArtImagePath: "assets/image/miagi3.jpg",
-      audioPath: "song/MiyaGi - Angel.mp3",
+      audioPath: "song/MiyaGi.mp3",
       songName: "Panda",
       artistName: "M I A G I",
     ),
   ];
+
+  //current song playing index
   int? _currentSongIndex ;
+
+ /*
+
+ A U D I O P L A Y E R
+
+*/
+ //audio player
+ final AudioPlayer _audioPlayer = AudioPlayer();
+
+ //durations
+ Duration _currentDuration = Duration.zero;
+ Duration _totalDuration = Duration.zero;
+
+ //constructor
+ PlaylistProvider(){
+  listenToDuration();
+  }
+
+ //initiallity not playing
+
+bool _isPlaying = false;
+ //play the song 
+void play () async {
+  final String path =_playlist[_currentSongIndex!].audioPath;
+  await _audioPlayer.stop(); // stop current song 
+  await _audioPlayer.play(AssetSource(path)); //play the new song
+  _isPlaying = true;
+  notifyListeners();
+}
+
+ //pause current song
+ void pause () async {
+  await _audioPlayer.pause();
+  _isPlaying = false;
+  notifyListeners();
+ }
+
+ //resume playing
+ void resume () async {
+  await _audioPlayer.resume();
+  _isPlaying = true;
+  notifyListeners();
+ }
+
+ //pause or resume
+ void pauseOrResume () async {
+ if(_isPlaying) {
+  pause();
+ }else{
+  resume();
+ }
+ notifyListeners();
+ }
+
+ //seek to a specitic position in the current song
+ void seek(Duration position) async {
+  await _audioPlayer.seek(position);
+ }
+
+ //play next song
+ void playNextSong () {
+  if(_currentSongIndex != null) {
+    if(_currentSongIndex! < _playlist.length -1) {
+      //go to th new song if it's not last song
+      currentSongIndex = _currentSongIndex! +1;
+    }else{
+      //if it's the last song ,loop back to the th first song
+      currentSongIndex = 0;
+    }
+  }
+ }
+
+ //play perevios song
+ void playPreviousSong () async {
+  //if more the 2 seconds have passed ,restart the current song
+  if(_currentDuration.inSeconds > 2) {
+    seek(Duration.zero);
+  }
+  //if it's within first 2 second of the song , go to previous song
+  else {
+    if(_currentSongIndex! > 0) {
+  currentSongIndex = _currentSongIndex! -1;
+    }else{
+      //if it's the fist song , loop back to last song
+      currentSongIndex = _playlist.length -1;
+    }
+  }
+ }
+
+ //listen to duration
+ void listenToDuration () {
+
+  //listen for total duration
+  _audioPlayer.onDurationChanged.listen((newDuration) { 
+    _totalDuration = newDuration;
+    notifyListeners();
+  });
+
+  //listen for current duration
+  _audioPlayer.onDurationChanged.listen((newPosition) { 
+    _currentDuration = newPosition;
+    notifyListeners();
+  });
+
+  //listenfor song completion
+  _audioPlayer.onDurationChanged.listen((event) {
+    playNextSong();
+   });
+
+ }
+
+ //dispose aduio player
+
+/*
+  G E T T E R S
+
+ */
 
   List <Song> get playlist => _playlist;
   int? get currentSongIndex=> _currentSongIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
+
+  /*
+
+ S E T T E R S 
+
+
+  */
 
 
 
   set currentSongIndex (int? newindex) {
+    //UPDATE CURRENT SONG INDEX
     _currentSongIndex = newindex;
 
+    if(newindex != null) {
+      play(); //play the song at the new index
+    }
+
+   //UPDATE UI
     notifyListeners();
   }
 }
--- music_project/lib/pages/song_pages.dart
+++ music_project/lib/pages/song_pages.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:music_project/components/neu_box.dart';
 import 'package:music_project/models/playlist_provider.dart';
 import 'package:provider/provider.dart';
 
 class SongPage extends StatelessWidget {
   const SongPage({super.key});
+//convert duration into min sec
+String formatTime (Duration duration) {
+  String twoDigitSeconds = duration.inSeconds.remainder(60).toString().padLeft(2, "0");
+  String formattedTime = "${duration.inMinutes}:$twoDigitSeconds";
+  return formattedTime;
+}
+
 
   @override
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
-      builder: (context, value, child) => Scaffold(
+      builder: (context, value, child) {
+        //get playlist
+       final playlist = value.playlist;
+        //get current song index
+      final currentSong = playlist[value.currentSongIndex ?? 0];
+        //return scaffold ui
+
+        return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+            padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25,top: 30),
             child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 //appBar
                 Row(
@@ -31,7 +47,7 @@ class SongPage extends StatelessWidget {
                     const Text(
                       "P L A Y L I S Y",
                       style: TextStyle(
-                        fontSize: 26,
+                        fontSize: 22,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
@@ -44,35 +60,35 @@ class SongPage extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 30,),
+               const Gap(30),
                 //album artwork
                 NeuBox(
                   child: Column(
                     children: [
                       ClipRRect(
                         child: Image.asset(
-                          "assets/image/miagi1.jpg",
+                          currentSong.albumArtImagePath
                         ),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.all(15),
+                       Padding(
+                        padding: const EdgeInsets.all(15),
                         child: Row(
                           children: [
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  "So Sick",
-                                  style: TextStyle(
+                                   currentSong.songName,
+                                  style: const TextStyle(
                                     fontWeight: FontWeight.bold,
                                     fontSize: 20,
                                   ),
                                 ),
-                                Text("Neyo"),
+                                 Text(currentSong.artistName),
                               ],
                             ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 210,bottom: 20),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 200,bottom: 20),
                               child: Icon(Icons.favorite,
                               color: Colors.red,
                               ),
@@ -88,17 +104,17 @@ class SongPage extends StatelessWidget {
                    Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 25.0,),
                     child: Column(
-                      children:[ const Row(
+                      children:[ Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                        //stars time 
-                       Text("0:00"),
+                       Text(formatTime(value.currentDuration)),
                        //shuffle icon
-                       Icon(Icons.shuffle),
+                       const Icon(Icons.shuffle),
                        //repeat icon
-                       Icon(Icons.repeat),
+                       const Icon(Icons.repeat),
                        //end time
-                        Text("0:00"),
+                        Text(formatTime(value.totalDuration)),
                         ],
                       ),
 
@@ -109,10 +125,15 @@ class SongPage extends StatelessWidget {
                       ),
                        child: Slider(
                         min: 0,
-                        max: 100,
-                        value: 50, 
+                        max: value.totalDuration.inSeconds.toDouble(),
+                        value: value.currentDuration.inSeconds.toDouble(),
                         activeColor: Colors.green,
-                        onChanged: (value) {
+                        onChanged: (double double) {
+                           //duration when the user is sliding around
+                          },
+                          onChangeEnd: (double double) {
+                          // sliding has finished , go to that poss=ition in song duration
+                          value.seek(Duration(seconds: double.toInt()));
                           },
                          ),
                         ),
@@ -120,47 +141,45 @@ class SongPage extends StatelessWidget {
                     ),
                   ),
                 //playBack
-                 Row(
-                  children: [
-                   Expanded(
-                     child: GestureDetector(
-                      onTap: () {},
-                       child: NeuBox(
+                const Gap(10),
+                  Row(
+                    children: [
+                      Expanded(child: 
+                      GestureDetector(
+                       onTap:value.playPreviousSong,
+                       child: const NeuBox(
                         child: Icon(Icons.skip_previous),
+                        ),
                        ),
-                     ),
-                   )
-                  ],
-                 ),
-                   Row(
-                  children: [
-                   Expanded(
-                     child: GestureDetector(
-                      onTap: () {},
-                       child: NeuBox(
-                        child: Icon(Icons.play_arrow),
+                      ),
+                     const Gap(20),
+                      Expanded(
+                        flex: 2,
+                        child: 
+                      GestureDetector(
+                       onTap: value.pauseOrResume,
+                       child:  NeuBox(
+                        child: Icon(value.isPlaying ? Icons.pause:Icons.play_arrow),
+                        ),
                        ),
-                     ),
-                   )
-                  ],
-                 ),
-                    Row(
-                  children: [
-                   Expanded(
-                     child: GestureDetector(
-                      onTap: () {},
-                       child: NeuBox(
-                        child: Icon(Icons.play_arrow),
+                      ),
+                     const Gap(20),
+                      Expanded(child: 
+                      GestureDetector(
+                       onTap: value.playNextSong,
+                       child: const NeuBox(
+                        child: Icon(Icons.skip_next),
+                        ),
                        ),
-                     ),
-                   )
-                  ],
+                      ),
+                    ],
                  ),
               ],
             ),
           ),
         ),
-      ),
+      );
+      }
     );
   }
 }
